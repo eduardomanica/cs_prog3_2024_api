a3d1f481af9faa110f1523bf56791bceb75de6d1
Eduardo Manica <20221PF.INF_I0005@passofundo.ifsul.local>
2024-08-20 12:05:57 -0300

    update, delete e insert

--- servidor.js
+++ servidor.js
@@ -128,6 +128,79 @@ sw.get('/listjogadores', function (req, res, next) {
     });
 });
 
+sw.post('/insertpatentes', function (req, res, next) {
+    postgres.connect(function(err,client,done) {
+        if(err){
+            console.log("Não conseguiu acessar o BD"+ err);
+            res.status(400).send('{'+err+'}');
+        }else{
+            var q1 ={
+                text: 'insert into tb_patente (nome, quant_min_pontos, datacriacao, cor, logotipo) values ($1, $2, now(), $3, $4) returning codigo, nome, quant_min_pontos, datacriacao, cor, logotipo',
+                values :[req.body.nome, req.body.quant_min_pontos, req.body.cor, req.body.logotipo]
+            }
+
+            client.query(q1, function(err,result1) {
+                if(err){
+                    console.log('retornou 400 no insert q1');
+                    res.status(400).send('{'+err+'}');
+                }else{
+                    console.log('retornou 201 no insertpatentes');
+                    res.status(201).send({"codigo": result1.rows[0].codigo, "nome": result1.rows[0].nome,
+                     "quant_min_pontos" : result1.rows[0].quant_min_pontos, "cor" : result1.rows[0].cor,
+                    "datacriacao" : result1.rows[0].datacriacao, "logotipo" : result1.rows[0].logotipo});
+                }
+            })
+        }
+    })
+})
+
+sw.post('/updatepatentes', function (req, res, next) {
+    postgres.connect(function(err,client,done) {
+        if(err){
+            console.log("Não conseguiu acessar o BD"+ err);
+            res.status(400).send('{'+err+'}');
+        }else{
+            var q1 ={
+                text: 'update tb_patente set nome=$1, quant_min_pontos=$2, cor=$3, logotipo=$4 where tb_patente.codigo=$5  returning codigo, nome, quant_min_pontos, to_char(datacriacao, \'dd/mm/yyyy hh24:mm:ss\') as datcriacao, cor, logotipo',
+                values :[req.body.nome, req.body.quant_min_pontos, req.body.cor, req.body.logotipo, req.body.codigo]
+            }
+
+            client.query(q1, function(err,result1) {
+                if(err){
+                    console.log('retornou 400 no update q1');
+                    res.status(400).send('{'+err+'}');
+                }else{
+                    console.log('retornou 201 no updatepatentes');
+                    res.status(201).send({"nome": result1.rows[0].nome,"quant_min_pontos" : result1.rows[0].quant_min_pontos, 
+                    "cor" : result1.rows[0].cor, "datacriacao" : result1.rows[0].datacriacao,
+                     "logotipo" : result1.rows[0].logotipo,"codigo": result1.rows[0].codigo});
+                }
+            })
+        }
+    })
+})
+
+sw.get('/deletepatentes/:codigo', function(req, res, next){
+    postgres.connect(function (err, client, done) {
+        if (err) {
+            console.log("Não conseguiu acessar o banco de dados!" + err);
+            res.status(400).send('{' + err + '}');
+        } else {
+
+            var q = {
+                text: 'delete FROM tb_patente where codigo = $1',
+                values: [req.params.codigo]
+            }}
+            client.query(q, function(err, result){
+                if(err){
+                    console.log(err);
+                    res.status(400).send('{' + err + '}');
+                }else{
+                    res.status(200).send({'codigo': req.params.codigo})
+                }
+            })
+        })
+})
 
 sw.listen(4000, function () {
     console.log('Server is running.. on Port 4000');
